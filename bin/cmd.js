b9530a6cf274f5edf9328030f405e16ce6a64508
Dan Flettre <fletd01@yahoo.com>
2015-03-28 01:59:47 -0500

    rework standard

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,115 +1,12 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var minimist = require('minimist')
-var standard = require('../')
-var standardFormat = require('standard-format')
-var stdin = require('get-stdin')
-
-var argv = minimist(process.argv.slice(2), {
-  alias: {
-    format: 'F',
-    help: 'h',
-    verbose: 'v'
-  },
-  boolean: [
-    'format',
-    'help',
-    'stdin',
-    'verbose',
-    'version'
-  ]
-})
-
-// running `standard -` is equivalent to `standard --stdin`
-if (argv._[0] === '-') {
-  argv.stdin = true
-  argv._.shift()
-}
-
-if (argv.help) {
-  console.log(function () {
-  /*
-  standard - JavaScript Standard Style
-
-  Usage:
-      standard <flags> [FILES...]
-
-      If FILES is omitted, then all JavaScript source files (*.js, *.jsx) in the current
-      working directory are checked, recursively.
-
-      Certain paths (node_modules/, .git/, coverage/, *.min.js, bundle.js) are
-      automatically excluded.
-
-  Flags:
-      -F  --format    Automatically format code. (using standard-format)
-      -v, --verbose   Show error codes. (so you can ignore specific rules)
-          --stdin     Read file text from stdin.
-          --version   Show current version.
-      -h, --help      Show usage information.
-
-  Readme:  https://github.com/feross/standard
-  Report bugs:  https://github.com/feross/standard/issues
-
-  */
-  }.toString().split(/\n/).slice(2, -2).join('\n'))
-  process.exit(0)
-}
-
-if (argv.version) {
-  console.log(require('../package.json').version)
-  process.exit(0)
-}
-
-if (argv.stdin) {
-  stdin(function (text) {
-    if (argv.format) {
-      text = standardFormat.transform(text)
-      process.stdout.write(text)
-    }
-    standard.lintText(text, onResult)
-  })
-} else {
-  var lintOpts = {}
-  if (argv.format) {
-    lintOpts._onFiles = function (files) {
-      files.forEach(function (file) {
-        var data = fs.readFileSync(file).toString()
-        fs.writeFileSync(file, standardFormat.transform(data))
-      })
-    }
+var opts = {
+  tagline: 'JavaScript Custom Style',
+  readme: 'https://github.com/flet/standard-engine',
+  bugs: 'https://github.com/flet/standard-engine/issues',
+  eslintConfig: {
+    useEslintrc: false
   }
-  standard.lintFiles(argv._, lintOpts, onResult)
 }
 
-function onResult (err, result) {
-  if (err) return error(err)
-  if (result.errorCount === 0) process.exit(0)
-
-  console.error(
-    'Error: Use JavaScript Standard Style ' +
-    '(https://github.com/feross/standard)'
-  )
-
-  result.results.forEach(function (result) {
-    result.messages.forEach(function (message) {
-      console.error(
-        '  %s:%d:%d: %s%s',
-        result.filePath, message.line || 0, message.column || 0, message.message,
-        argv.verbose ? ' (' + message.ruleId + ')' : ''
-      )
-    })
-  })
-
-  process.exit(1)
-}
-
-function error (err) {
-  console.error('Unexpected Linter Output:\n')
-  console.error(err.stack || err.message || err)
-  console.error(
-    '\nIf you think this is a bug in `standard`, open an issue: ' +
-    'https://github.com/feross/standard'
-  )
-  process.exit(1)
-}
+require('../lib/cli.js')(opts)
--- test/test.js
+++ test/test.js
@@ -21,41 +21,13 @@ var argv = minimist(process.argv.slice(2), {
 })
 
 var TMP = path.join(__dirname, 'tmp')
-var STANDARD = path.join(__dirname, 'bin', 'cmd.js')
+var STANDARD = path.join(__dirname, 'standard-cmd.js')
 
 var urls = [
+  'https://github.com/feross/standard.git',
   'https://github.com/beatgammit/base64-js.git',
   'https://github.com/brandonhorst/coverage-test.git',
-  'https://github.com/brandonhorst/empty.git',
-  'https://github.com/feross/bittorrent-dht.git',
-  'https://github.com/feross/bittorrent-protocol.git',
-  'https://github.com/feross/bittorrent-tracker.git',
-  'https://github.com/feross/buffer.git',
-  'https://github.com/feross/magnet-uri.git',
-  'https://github.com/feross/parse-torrent.git',
-  'https://github.com/feross/simple-peer.git',
-  'https://github.com/feross/studynotes.git',
-  'https://github.com/feross/webtorrent.git',
-  'https://github.com/Flet/dailyconnect.git',
-  'https://github.com/Flet/exitzero.git',
-  'https://github.com/mafintosh/hms-protocol.git',
-  'https://github.com/mafintosh/hms.git',
-  'https://github.com/mafintosh/json-format-stream.git',
-  'https://github.com/mafintosh/level-enumerate.git',
-  'https://github.com/mafintosh/level-logs.git',
-  'https://github.com/mafintosh/level-temp.git',
-  'https://github.com/mafintosh/node-gyp-install.git',
-  // 'https://github.com/mafintosh/peerflix.git', // still using standard v2
-  'https://github.com/mafintosh/swap-to-level.git',
-  'https://github.com/mafintosh/telephone.git',
-  'https://github.com/mafintosh/what-line-is-this.git',
-  'https://github.com/maxogden/standard-format.git',
-  // 'https://github.com/ngoldman/gh-release.git', // still using standard v2
-  'https://github.com/ngoldman/magnet-link.git',
-  'https://github.com/ngoldman/wireframe.css.git',
-  'https://github.com/npm/fstream.git',
-  // 'https://github.com/npm/npm.git' // in progress
-  'https://github.com/othiym23/packard.git'
+  'https://github.com/brandonhorst/empty.git'
 ]
 
 var modules = {}
