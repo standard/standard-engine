7576c9e0c6bf305cb6670c16c103df1d71eec438
Linus Unnebäck <linus@folkdatorn.se>
2020-04-30 09:08:36 +0100

    Use absolute path in tests

--- test/api.js
+++ test/api.js
@@ -1,5 +1,6 @@
 var eslint = require('eslint')
 var Linter = require('../').linter
+var path = require('path')
 var test = require('tape')
 
 function getStandard () {
@@ -14,7 +15,7 @@ function getStandard () {
 test('api: lintFiles', function (t) {
   t.plan(3)
   var standard = getStandard()
-  standard.lintFiles([], { cwd: '../bin' }, function (err, result) {
+  standard.lintFiles([], { cwd: path.join(__dirname, '../bin') }, function (err, result) {
     t.error(err, 'no error while linting')
     t.equal(typeof result, 'object', 'result is an object')
     t.equal(result.errorCount, 0)
